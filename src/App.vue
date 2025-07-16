88db83a4a8e72c601d2a72cdedbfc148ee2bfd4b
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2025-07-16 08:31:28 +0200

    Podpięcie skryptu i pierwszy komponent i działające skrypty w VUE

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>Moje ziomy</h2>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: 'manuel',
+          name: 'Manuel Lorenz',
+          phone: '0 123455',
+          email: 'manuel@mail.com',
+        },
+        {
+          id: 'franko',
+          name: 'Franek Bojda',
+          phone: '999 000',
+          email: 'franko@mail.com',
+        },
+      ]
+    }
+  }
+}
+</script>
